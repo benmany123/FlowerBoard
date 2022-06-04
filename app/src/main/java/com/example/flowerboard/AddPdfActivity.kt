360eba4490d7460d8f396d5d7db7339d5302cf06
Chan Cheuk Wai <benmany123@gmail.com>
2022-06-04 11:52:22 +0800

    third commit

---
+++ app/src/main/java/com/example/flowerboard/AddPdfActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flowerboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPdfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pdf)
+    }
+}
