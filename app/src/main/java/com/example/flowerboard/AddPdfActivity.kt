feb385c1aa616888c6e141052219f18046ed73d8
Chan Cheuk Wai <benmany123@gmail.com>
2022-06-04 17:03:42 +0800

    Fifth commit

--- app/src/main/java/com/example/flowerboard/AddPdfActivity.kt
+++ app/src/main/java/com/example/flowerboard/AddPdfActivity.kt
@@ -1,11 +1,268 @@
 package com.example.flowerboard
 
+import android.app.AlertDialog
+import android.app.Application
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.ColorSpace
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.flowerboard.databinding.ActivityAddPdfBinding
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 
 class AddPdfActivity : AppCompatActivity() {
+
+    //setup view binding activity_pdf_add -->ActivityPDFAdding Binding
+    private lateinit var binding: ActivityAddPdfBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    //progress dialog
+    private lateinit var progressDialog: ProgressDialog
+
+    //arraylist to hold pdf categories
+    private lateinit var categoryArrayList: ArrayList<modelCat>
+
+    //uri of picked pdf
+    private var pdfUri: Uri? = null
+
+    //TAG
+    private val TAG ="PDF_ADD_TAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_pdf)
+        binding = ActivityAddPdfBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        loadPdfCategories()
+
+        //setup progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //handle click, go back
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+        //handle click, show category pick dialog
+        binding.categoryTv.setOnClickListener {
+            categoryPickDialog()
+        }
+
+        //handle click, pick pdf intent
+        binding.PdfAttachButton.setOnClickListener {
+            pdfPickIntent()
+        }
+
+        //handle click, start uploading pdf/product
+        binding.submitButton.setOnClickListener {
+            //1. validate data
+            //2. upload pdf to firebase storage
+            //3. get url of uploaded pdf
+            //4. upload pdf info to firebase db
+
+            validateData()
+        }
     }
+
+    private var title = ""
+    private var description = ""
+    private var category = ""
+
+    private fun validateData() {
+        //1. Validate data
+        Log.d(TAG, "validateData: Validating data")
+
+        //get data
+        title = binding.titleEt.text.toString().trim()
+        description = binding.descriptionEdit.text.toString().trim()
+        category = binding.categoryTv.text.toString().trim()
+
+        //validate data
+        if (title.isEmpty()){
+            Toast.makeText(this, "Enter Title ...", Toast.LENGTH_SHORT).show()
+        }
+        else if(description.isEmpty()){
+            Toast.makeText(this, "Enter Description ...", Toast.LENGTH_SHORT).show()
+        }
+        else if (category.isEmpty()){
+            Toast.makeText(this, "Enter Category ...", Toast.LENGTH_SHORT).show()
+        }
+        else if (pdfUri == null){
+            Toast.makeText(this, "Pick PDF ...", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            //data validated, begin upload
+            uploadPdf()
+        }
+    }
+
+    private fun uploadPdf() {
+        //2.  Upload pdf to firebase storage
+        Log.d(TAG, "uploadPdf: Uploading to storage...")
+
+        //show progress dialog
+        progressDialog.setMessage("Uploading PDF...")
+        progressDialog.show()
+
+        //timestamp
+        val timestamp = System.currentTimeMillis()
+
+        //path of pdf in firebase storage
+        val filePathAndName = "FlowerBoards/$timestamp"
+        //storage reference
+        val storageReference = FirebaseStorage.getInstance().getReference(filePathAndName)
+        storageReference.putFile(pdfUri!!)
+            .addOnSuccessListener { taskSnapshot ->
+                Log.d(TAG, "uploadPdf: PDF upload now getting url...")
+
+                //3. Get url of uploaded pdf
+                val uriTask: Task<Uri> = taskSnapshot.storage.downloadUrl
+                while (!uriTask.isSuccessful);
+                val uploadPdfUrl = "${uriTask.result}"
+
+                uploadPdfDB(uploadPdfUrl, timestamp)
+            }
+            .addOnFailureListener {e->
+                Log.d(TAG, "uploadPdf: failed to upload due to ${e.message}")
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to upload due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadPdfDB(uploadPdfUrl: String, timestamp: Long) {
+        //4. Upload PDF info to firebase db
+        Log.d(TAG, "uploadPdfDB: uploading to db")
+        progressDialog.setMessage("Uploading pdf Info...")
+
+        //uid of current user
+        val uid = firebaseAuth.uid
+
+        //setup data to upload
+        val hashMap: HashMap<String, Any> = HashMap()
+        hashMap["uid"] = "$uid"
+        hashMap["id"] = "$timestamp"
+        hashMap["title"] = "$title"
+        hashMap["description"] = "$description"
+        hashMap["categoryId"] = "$selectedCategoryId"
+        hashMap["url"] = "$uploadPdfUrl"
+        hashMap["timestamp"] = timestamp
+        hashMap["viewsCount"] = 0
+        hashMap["downloadsCount"] = 0
+
+        //db reference DB > Flower Boards > Product ID > Flower Board Info
+        val ref = FirebaseDatabase.getInstance().getReference("FlowerBoards")
+        ref.child("$timestamp")
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                Log.d(TAG, "uploadPdfDB: Uploaded to db")
+                progressDialog.dismiss()
+                Toast.makeText(this, "Uploaded...", Toast.LENGTH_SHORT).show()
+                pdfUri = null
+            }
+            .addOnFailureListener {e->
+                Log.d(TAG, "uploadPdfDB: failed to upload due to ${e.message}")
+                progressDialog.dismiss()
+                Toast.makeText(this, "Failed to upload due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun loadPdfCategories() {
+        Log.d(TAG, "loadPdfCategories: Loading PDF categories")
+        //init arraylist
+        categoryArrayList = ArrayList()
+
+        //db reference to load categories DF > Categories
+        val ref = FirebaseDatabase.getInstance().getReference("Categories")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clear list before adding data
+                categoryArrayList.clear()
+                for(ds in snapshot.children){
+                    //get data
+                    val model = ds.getValue(modelCat::class.java)
+                    //add to array list
+                    categoryArrayList.add(model!!)
+                    Log.d(TAG, "onDataChange: ${model.category}")
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
+    private var selectedCategoryId =""
+    private var selectedCategoryTitle =""
+
+    private fun categoryPickDialog(){
+        Log.d(TAG, "categoryPickDialog: Showing pdf category pick dialog")
+
+        //get string array of categories from arraylist
+        val categoriesArray = arrayOfNulls<String>(categoryArrayList.size)
+        for (i in categoryArrayList.indices){
+            categoriesArray[i] = categoryArrayList[i].category
+        }
+
+        //alert dialog
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pick Category")
+            .setItems(categoriesArray){dialog, which ->
+                //handle item click
+                //get clicked item
+                selectedCategoryTitle = categoryArrayList[which].category
+                selectedCategoryId = categoryArrayList[which].id
+                //set category to text
+                binding.categoryTv.text = selectedCategoryTitle
+
+                Log.d(TAG, "categoryPickDialog: Selected Category ID : $selectedCategoryId")
+                Log.d(TAG, "categoryPickDialog: Selected Category Title : $selectedCategoryTitle")
+            }
+            .show()
+    }
+
+    private fun pdfPickIntent(){
+        Log.d(TAG, "pdfPickIntent: starting pdf pick intent")
+
+        val intent = Intent()
+        //intent.type = "application/pdf"
+        intent.type = "image/png"
+        intent.action = Intent.ACTION_GET_CONTENT
+        pdfActivityResultLauncher.launch(intent)
+
+    }
+
+    val pdfActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult>{ result ->
+            if(result.resultCode == RESULT_OK){
+                Log.d(TAG, "PDF Picked:: ")
+                pdfUri = result.data!!.data
+            }
+            else{
+                Log.d(TAG, "PDF Pick cancelled: ")
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    )
 }
